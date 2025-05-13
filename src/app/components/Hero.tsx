caf1614bb6770742d497fa346dc7700f39280f9e
JFarah <joaquinefarah@gmail.com>
2025-05-13 13:10:13 -0300

    Enhance HeroSection content and layout for improved clarity and user engagement

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -9,14 +9,14 @@ export default function HeroSection() {
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
-              <p className="text-base/7 font-semibold text-amber-500">ProAlianza</p>
+
+              <p className="text-base/7 font-semibold text-amber-500 text-center">ProAlianza</p>
               <h1 className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl text-center">
               ¿QUIÉNES SOMOS?
               </h1>
               <p className="mt-20 text-3xl text-gray-700 text-center">
               <span className="bg-amber-500 font-bold">Pro Alianza®</span> es una empresa de <span className="bg-amber-500 font-bold">Servicios de Asistencia Técnica, Capacitación, Entrenamiento, Calibración de Sistemas de Detección y Comercialización de Productos</span> para la Seguridad bajo el esquema de Sociedad de Acciones Simplificada (S.A.S). Brinda un servicio de calidad en su tipo contando a su vez con un equipo interdisciplinario con 30 años de experiencia en funciones claves de diversas empresas.
               </p>
-
 
               <h1 className="mt-50 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl text-center">
               NUESTRA MISIÓN
@@ -25,13 +25,18 @@ export default function HeroSection() {
                Volcar nuestra experiencia en el análisis de operaciones industriales, con el fin de obtener su <span className="bg-amber-500 font-bold">optimización</span>, mejorando la eficiencia y la seguridad, al mismo tiempo que se reducen los costos.
               </p>
 
+              <h1 className="mt-50 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl text-center">
+              NUESTRO CRITERIO INDUSTRIAL
+              </h1>
+              <p className="mt-20 text-3xl text-gray-700 text-center">
+              Se basa en la optimización de procesos y recursos, aplicando las mejores prácticas y tecnologías disponibles. Nos enfocamos en la seguridad, eficiencia y sostenibilidad de las operaciones, buscando siempre el equilibrio entre el rendimiento operativo y la gestión responsable de los recursos. Trabajamos de manera <span className="bg-amber-500 font-bold">proactiva</span> para identificar áreas de mejora y proponer soluciones innovadoras que agreguen valor real a nuestros clientes.
+              </p>
+
 
 
             </div>
           </div>
-        </div>    
-        
-            
+        </div>   
         
         <div className="-mt-10 -ml-12 p-12 lg:sticky lg:top-20 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
          
@@ -44,8 +49,6 @@ export default function HeroSection() {
         />
         </div>
       </div>
-
-      
     </div>
   )
 }
