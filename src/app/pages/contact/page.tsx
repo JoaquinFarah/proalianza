1e3aecfbfb0ee15a10a1c2e02832090c2c9bd289
JFarah <joaquinefarah@gmail.com>
2025-05-13 17:50:13 -0300

    Update text shadow color and content in ContactPage for improved clarity

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -20,7 +20,7 @@ export default function ContactPage() {
           className="absolute top-0 left-0 w-full h-full object-cover shadow-xl" 
         />
         <div className="absolute top-1/3 left-1/5 transform -translate-x-1/7 -translate-y-1 text-white text-5xl font-semibold text-center rounded-lg bg-gray-900/50 p-5 backdrop-blur-xs">
-          <p className="uppercase" style={{ textShadow: '2px 2px 4px #e5771b' }}>Comunicate con nosotros y adquirí de manera integral todos los datos y soluciones eficiencientes para tu proyecto</p>
+          <p className="uppercase" style={{ textShadow: '2px 2px 4px #f7950a' }}>Comunicate con nosotros Somos la solución integral y eficiente para tu proyecto</p>
         </div>
       </div>
     <ContactForm />
