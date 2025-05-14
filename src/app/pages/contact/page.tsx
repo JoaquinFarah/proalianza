8d9229a481d0d57e6a58ea2976db84c22c50c234
JFarah <joaquinefarah@gmail.com>
2025-05-14 12:12:27 -0300

    Update ContactPage image brightness and remove text shadow for improved readability

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -17,10 +17,10 @@ export default function ContactPage() {
           alt="Icono" 
           width={2000} 
           height={2000} 
-          className="absolute top-0 left-0 w-full h-full object-cover shadow-xl" 
+          className="absolute top-0 left-0 w-full h-full object-cover shadow-xl brightness-65" 
         />
         <div className="absolute top-1/3 left-1/5 transform -translate-x-1/7 -translate-y-1 text-white text-5xl font-semibold text-center rounded-lg bg-gray-900/50 p-5 backdrop-blur-xs">
-          <p className="uppercase" style={{ textShadow: '2px 2px 4px #f7950a' }}>Comunicate con nosotros Somos la solución integral y eficiente para tu proyecto</p>
+          <p className="uppercase">Comunicate con nosotros Somos la solución integral y eficiente para tu proyecto</p>
         </div>
       </div>
     <ContactForm />
