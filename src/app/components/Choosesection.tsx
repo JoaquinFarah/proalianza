54a1cd105f4be6338c70258e95b03c2e9d4d81cc
JFarah <joaquinefarah@gmail.com>
2025-05-14 13:48:24 -0300

    Add ChooseSection component to ProfilePage for enhanced content presentation

---
+++ src/app/components/Choosesection.tsx
@@ -0,0 +1,100 @@
+import Image from 'next/image'
+
+export default function ChooseSection() {
+  return (
+    <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0 -mr-10">
+      {/* Sticky background */}
+      <div
+        className="pointer-events-none absolute left-0 right-0 -z-10"
+        style={{
+          top: '0px', 
+          bottom: 0,
+          backgroundImage: "url('/fondo3.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'top',
+          backgroundAttachment: 'fixed',
+          backgroundRepeat: 'no-repeat',
+          opacity: 1 // Opacity adjust
+        }}
+        aria-hidden="true"
+      />
+
+
+      <div>
+            <h1 style={{ backgroundColor: '#f7950a' }} className="text-5xl flex justify-center items-center mt-20 p-5 uppercase font-bold">¿Por qué elegirnos?</h1>
+            <div className="text-3xl flex justify-center text-center items-center mt-10 px-55">
+                <p>
+                En un mundo donde la seguridad y la eficiencia son primordiales, somos <span style={{ backgroundColor: '#f7950a' }} className="font-bold"> tu nuevo</span> aliado estratégico. Con 30 años de experiencia en la industria, ofrecemos soluciones integrales que garantizan la protección de los trabajadores y la optimización de los recursos.
+                </p>
+            </div>   
+
+            {/* Three Columns */}
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-20 px-10">
+            <div className="flex flex-col items-center text-center">
+                <Image 
+                src="/Risk.svg" 
+                alt="High Risk Company 1" 
+                width={100} 
+                height={100} 
+                className="rounded-lg"                
+                />
+                <h2 style={{ backgroundColor: '#f7950a' }} className="text-2xl font-semibold mt-5 p-2">Gestión de Riesgos</h2>
+                <p className="mt-3 text-lg">Ofrecemos soluciones avanzadas para identificar y mitigar riesgos en entornos laborales de alto peligro</p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+                <Image 
+                src="/Protectionico.svg" 
+                alt="High Risk Company 2" 
+                width={100} 
+                height={100} 
+                className="rounded-lg"
+                />
+                <h2 style={{ backgroundColor: '#f7950a' }} className="text-2xl font-semibold mt-5 p-2">Seguridad Industrial</h2>
+                <p className="mt-3 text-lg">Implementamos estrategias de seguridad industrial que protegen a los trabajadores y optimizan la productividad</p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+                <Image 
+                src="/Special.svg" 
+                alt="High Risk Company 3" 
+                width={100} 
+                height={100} 
+                className="rounded-lg"
+                />
+                <h2 style={{ backgroundColor: '#f7950a' }} className="text-2xl font-semibold mt-5 p-2">Capacitación Especializada</h2>
+                <p className="mt-3 text-lg">Brindamos formación especializada para garantizar que los equipos estén preparados para enfrentar desafíos críticos</p>
+            </div>
+            </div>
+
+            {/* Two Columns */}
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-20 px-50">
+            <div className="flex flex-col items-center text-center">
+                <Image 
+                src="/Hat.svg" 
+                alt="High Risk Company 1" 
+                width={100} 
+                height={100} 
+                className="rounded-lg"
+                />
+                <h2 style={{ backgroundColor: '#f7950a' }} className="text-2xl font-semibold mt-5 p-2">Materiales de Óptima calidad</h2>
+                <p className="mt-3 text-lg">Los mejores materiales disponibles en el mercado para garantizar la excelencia tanto a nuestro personal como a nuestros clientes para asegurarles la tranquilidad para cumplir con las exigencias de los mismos</p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+                <Image 
+                src="/Hands.svg" 
+                alt="High Risk Company 2" 
+                width={100} 
+                height={100} 
+                className="rounded-lg"
+                />
+                <h2 style={{ backgroundColor: '#f7950a' }} className="text-2xl font-semibold mt-5 p-2">Experiencia comprobable</h2>
+                <p className="mt-3 text-lg">Porque nuestro staff ha pasado los últimos 20 años en áreas de seguridad laboral y capacitaciones permanentes, estamos  seguros que podemos ofrecer todo lo que sabemos. Cuando hay conocimiento se hace notar </p>
+            </div>          
+            </div>
+        </div>
+
+           
+    </div>
+  )
+}
