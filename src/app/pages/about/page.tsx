3b7affc5bc7b7444288c866a0394cc41553f9028
JFarah <joaquinefarah@gmail.com>
2025-06-02 09:44:12 -0300

    Enhance text styling in AboutPage component

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -20,7 +20,7 @@ export default function AboutPage() {
           className="absolute top-0 left-0 w-full h-full object-cover shadow-xl brightness-65" 
         />
         <div className="absolute top-3/5 left-1/5 transform -translate-x-1/5 -translate-y-1 text-white text-4xl font-semibold text-center rounded-lg bg-gray-900/50 p-5 backdrop-blur-xs w-full" >
-          <p className="uppercase" >SOMOS ESPECIALISTAS EN LA BÚSQUEDA Y OBTENCIÓN DEL LÍMITE TÉCNICO </p>
+          <p className="uppercase" >SOMOS <span style={{ backgroundColor: '#f7950a' }} className=" text-white px-1 rounded">ESPECIALISTAS</span> EN LA BÚSQUEDA Y OBTENCIÓN DEL LÍMITE TÉCNICO </p>
           <p>TANTO DEL PERSONAL COMO DE LAS HERRAMIENTAS Y EQUIPOS</p>
         </div>
       </div>      
